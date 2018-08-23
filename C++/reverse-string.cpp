06c4ac2996f81c7ab5f35aaf1178760611b6f1e5
timtingwei <timtingwei@hotmail.com>
2018-08-24 06:21:51 +0800

    Update reverse-string.cpp

--- C++/reverse-string.cpp
+++ C++/reverse-string.cpp
@@ -2,8 +2,11 @@
 // reverse-string.cpp
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
+/*
+// 创建新串
 // Time:O(n), Space:O(n);
 class Solution {
  public:
@@ -15,10 +18,42 @@ class Solution {
     return str;
   }
 };
+*/
+
+
+/*
+// 折半交换
+// Time: O(n), Space: O(1)
+class Solution {
+ public:
+  string reverseString(string s) {
+    int n = s.length();
+    for (int i = 0; i <= floor(n/2)-1; i++) {
+      char tmp = s[i]; s[i] = s[n-1-i];; s[n-1-i] = tmp;
+    }
+    return s;
+  }
+};
+*/
+
+// two pointers
+// Time: O(n), Space: O(1)
+class Solution {
+ public:
+  string reverseString(string s) {
+    int n = s.length();
+    int i = 0, j = n-1;
+    while (i < j) {
+      int tmp = s[i]; s[i] = s[j]; s[j] = tmp;
+      i++; j--;
+    }
+    return s;
+  }
+};
 
 int main() {
   Solution s;
-  string str = "hello";
+  string str = "A man, a plan, a canal: Panama";
   cout << s.reverseString(str) << endl;
   return 0;
 }
