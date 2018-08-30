ef75435aba897d8bfa37466204531705994d3d5b
timtingwei <timtingwei@hotmail.com>
2018-08-30 18:54:54 +0800

    Update first-unique-character-in-a-string.cpp

--- C++/first-unique-character-in-a-string.cpp
+++ C++/first-unique-character-in-a-string.cpp
@@ -4,25 +4,32 @@
 #include <string>
 using namespace std;
 
+
+// Time:O(n), Space:O(1)
 class Solution {
  public:
   int firstUniqChar(string s) {
-    char chars[200];
-    memset(chars, -1, sizeof(chars));
+    int cnt[30];
+    memset(cnt, 0, sizeof(cnt));
     for (int i = 0; i < s.length(); i++) {
-      cout << "i = " << i << endl;
-      if (chars[s[i] - 'a'] == -1) {
-        chars[s[i] - 'a'] = i;
-      }
+      cnt[s[i] - 'a']++;
+    }
+    for (int i = 0; i < s.length(); i++) {
+      if (cnt[s[i] - 'a'] == 1) return i;
     }
     return -1;
   }
 };
 
+
 int main() {
   Solution solution;
   string s1 = "leetcode", s2 = "loveleetcode";
+  string s3 = "uindrseqbljlhqvlwvgdebeihttirikuahlikgnahvgnptmqwbovmuwesxkvcitcwrwrucsbbfqvldridfviduqvmfcmeiphoqupbitnwdbvevouoaetisdmgvvvwoglwtgjrpcbghxkrkjthetxeexbphbjiehaicuicgnirslhdstgmqcdnlulpdpadjdltfouwhfqicfcqntnpeqaohslwkhbvflxaudembsrsindluthxapnmrqins";
+  string s4 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
   cout << solution.firstUniqChar(s1) << endl;
   cout << solution.firstUniqChar(s2) << endl;
+  cout << solution.firstUniqChar(s3) << endl;
+  cout << solution.firstUniqChar(s4) << endl;
   return 0;
 }
