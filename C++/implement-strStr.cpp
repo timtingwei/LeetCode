5a04bc15d657126c4c5e8e21f0f4187e68c406d3
timtingwei <timtingwei@hotmail.com>
2018-09-01 16:38:20 +0800

    Update implement-strStr.cpp

--- C++/implement-strStr.cpp
+++ C++/implement-strStr.cpp
@@ -6,7 +6,7 @@
 #include <string>
 using namespace std;
 
-
+/*
 // Time:O(n*m), Space:O(1)
 class Solution {
  public:
@@ -32,6 +32,21 @@ class Solution {
     return -1;
   }
 
+};
+*/
+
+// Time:O(n*m), Space:O(1)
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {
+    for (int i = 0; ; i++) {
+      for (int j = 0; ; j++) {
+        if (j == needle.length()) return i;
+        if (i+j == haystack.length()) return -1;
+        if (haystack[i+j] != needle[j]) break;
+      }
+    }
+  }
 };
 
 int main() {
