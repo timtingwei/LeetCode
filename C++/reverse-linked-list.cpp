3c8736839fa8aed6babcbdcfcba9567987b541b6
timtingwei <timtingwei@hotmail.com>
2018-09-02 22:44:15 +0800

    Update reverse-linked-list.cpp

--- C++/reverse-linked-list.cpp
+++ C++/reverse-linked-list.cpp
@@ -10,7 +10,7 @@
  * };
  */
 
-/*
+
 // Time:O(n^2), Space:O(1)
 class Solution {
  public:
@@ -35,10 +35,8 @@ class Solution {
   }
 };
 
-*/
 
-
-// follow1: iteratively
+// follow up1: iteratively
 class Solution {
  public:
   ListNode* reverseList(ListNode* head, int k) {
@@ -70,3 +68,59 @@ int k = 2;
 [2, 1, 3, 4, 5]
 */
 
+// follow up2: recursively
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head, int k) {
+    if (head == nullptr || k <= 1) return head;
+
+    ListNode *pNode = head, *reverseHead = nullptr,
+        *lastHead = head, *lastTail = nullptr;
+    for (;;) {
+      for (int i = 0; i < k-1; i++) {
+        if (pNode->next == nullptr) return reverseHead;
+        pNode = pNode->next;
+      }
+      ListNode *segHead = pNode, *nextNode = pNode->next;
+      if (reverseHead == nullptr) {
+        reverseHead = segHead;
+      }
+      if (lastTail != nullptr) {
+        lastTail->next = segHead;
+      }
+
+      while (pNode != lastHead) {
+        ListNode *p = lastHead;
+        while (p->next != pNode) {
+          p = p->next;
+        }
+        pNode->next = p;
+        pNode = p;
+      }
+      pNode->next = nextNode;
+      lastTail = lastHead;
+      lastHead = nextNode;
+      pNode = nextNode;
+      if (lastHead == nullptr) {
+        return reverseHead;
+      }
+    }
+    return reverseHead;
+  }
+};
+
+
+/*
+input:
+[1,2,3,4,5,6,7,8]
+3
+
+[3, 2, 1, 6, 5, 4, 7, 8]
+
+
+input:
+[1,2,3,4,5,6,7,8]
+2
+
+[2, 1, 4, 3, 6, 5, 8, 7]
+*/
