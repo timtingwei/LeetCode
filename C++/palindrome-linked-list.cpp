dc8266f3ce08ba57b2260701e805845e7bbddaaa
timtingwei <timtingwei@hotmail.com>
2018-09-11 17:36:25 +0800

    Create palindrome-linked-list.cpp

---
+++ C++/palindrome-linked-list.cpp
@@ -0,0 +1,33 @@
+// Copyright [2018] <mituh>
+// palindrome-linked-list.cpp
+
+class Solution {
+  bool isPalindrome(ListNode *head) {
+    if (head == nullptr) return false;
+    ListNode *head_a, *head_b, *pNode = head;
+    int len = 0;
+    while (pNode != nullptr) {
+      len++;
+      pNode = pNode->next;
+    }
+    if (len == 1) return true;
+
+    // int half_len = (int)len/2;
+    int half_len = len/2;
+    ListNode *prevNode = nullptr;
+    while (int i = 0; i < half_len; i++) {
+      ListNode *pNext = pNode->next;
+      pNode->next = prevNode;
+      prevNode = pNode;
+      pNode = pNext;
+    }
+    head_a = prevNode;
+    head_b = (len % 2) pNode->next : pNode;
+
+    while (head_a && head_b) {
+      if (head_a->val != head_b->val) return false;
+    }
+
+    return true;
+  }
+};
